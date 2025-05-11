0432071c539aa6cdf5730fb46e0a0b4329ea604a
mahfuzahmmedabir <mahfuz.abir06@gmail.com>
2025-05-11 14:27:15 +0100

    loop with recursion

---
+++ printf_one_to_five_with_recursion.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+void func(int i ){
+    if(i == 6){
+        return;
+    }
+    printf("%d\n", i);
+    func(i+1);
+}
+
+void five_to_one(int x){
+
+   if(x == 0){
+    return;
+   }
+   printf("%d \n", x);
+   five_to_one(x-1);
+}
+
+int main()
+{
+   int i = 1;
+   func(i);
+   printf("-----\n");
+   int x = 5;
+   five_to_one(x);
+   return 0;
+}
